040fbc7c44b9e30540d1afaaca9bed3d11adf488
Cameron Kurotori <cameron.kurotori@ibm.com>
2021-10-12 00:30:55 -0500

    fix log levels

--- logging/logging.go
+++ logging/logging.go
@@ -2,9 +2,11 @@ package logging
 
 import (
 	"os"
+	"strings"
 	"sync"
 
 	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 )
 
 var (
@@ -14,13 +16,26 @@ var (
 
 func GlobalLogger() log.Logger {
 	loggerInit.Do(func() {
-		globalLogger = NewLogger()
+		logger := NewLogger()
+		logger = log.With(logger, "caller", log.DefaultCaller, "ts", log.DefaultTimestamp)
+		globalLogger = logger
 	})
 	return globalLogger
 }
 
 func NewLogger() log.Logger {
 	logger := log.NewJSONLogger(os.Stderr)
-	logger = log.With(logger, "caller", log.DefaultCaller, "ts", log.DefaultTimestamp)
-	return logger
+
+	option := level.AllowInfo()
+	switch strings.ToLower(os.Getenv("LOGLEVEL")) {
+	case "debug":
+		option = level.AllowDebug()
+	case "info":
+		option = level.AllowInfo()
+	case "warn", "warning":
+		option = level.AllowWarn()
+	case "error":
+		option = level.AllowError()
+	}
+	return level.NewFilter(logger, option)
 }
