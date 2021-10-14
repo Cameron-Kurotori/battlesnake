7af4bc82d9002dabce0dd8a1cfd1fc2d714b4b15
Cameron Kurotori <cameron.kurotori@ibm.com>
2021-10-14 15:01:51 -0500

    scale food importance with avg len diff

--- logic.go
+++ logic.go
@@ -240,7 +240,7 @@ func (m heuristicMover) Move(state sdk.GameState) sdk.BattlesnakeMoveResponse {
 		foodDistRatio := foodAvailability(dir, state.You, state.Board)
 		foodExponent := 1.0
 		if state.You.Health < 60 || avgLenDiff > -1 {
-			foodExponent = 1 / float64(state.You.Health)
+			foodExponent = (avgLenDiff + 1) / (2.0 * float64(state.You.Health))
 			foodDistRatio = 1 - foodDistRatio
 		}
 		possibleMoves[dir].weight *= math.Pow(foodDistRatio, foodExponent)
