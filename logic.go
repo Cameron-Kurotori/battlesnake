ae64cd47678aaae3cfa28b7bc5a94601e9668786
Cameron Kurotori <cameron.kurotori@ibm.com>
2021-10-14 16:40:04 -0500

    more hungry

--- logic.go
+++ logic.go
@@ -275,8 +275,8 @@ func (m heuristicMover) Move(state sdk.GameState) sdk.BattlesnakeMoveResponse {
 
 		foodDistRatio := foodAvailability(dir, state.You, state.Board)
 		foodExponent := 1.0
-		if state.You.Health < 60 || avgLenDiff > -1 {
-			foodExponent = (avgLenDiff + 1) / (float64(state.You.Health))
+		if state.You.Health < 60 || avgLenDiff > -2 {
+			foodExponent = 2 * (avgLenDiff + 2) / (float64(state.You.Health))
 			foodDistRatio = 1 - foodDistRatio
 		}
 
